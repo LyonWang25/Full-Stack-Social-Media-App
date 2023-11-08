d0bdeff7f01d86634131ac7ee9206f394fc344cd
Lyon <t41589@gmail.com>
2023-11-08 17:44:09 -0500

    set up React-router-dom setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './globals.css';
+import SigninForm from './_auth/forms/SigninForm';
+import SignupForm from './_auth/forms/SignupForm';
+import { Home } from './_root/pages';
+import AuthLayout from './_auth/AuthLayout';
+import RootLayout from './_root/RootLayout';
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* public routes*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="/explore" element={<Explore />} />
+          <Route path="/saved" element={<Saved />} />
+          <Route path="/all-users" element={<AllUsers />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:id" element={<EditPost />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route path="/profile/:id/*" element={<Profile />} />
+          <Route path="/update-profile/:id" element={<UpdateProfile />} /> */}
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
